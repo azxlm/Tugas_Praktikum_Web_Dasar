a618c50f9220745120acfbd1893bfcb1d23b046e
azxlm <123433894+azxlm@users.noreply.github.com>
2025-10-24 08:49:49 +0700

    Update and rename index.py to main.py

--- index.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi Dunia")
-print("Perkenalkan nama saya Azis")
-print("Saya sekarang sedang mengalamai Butterfly Era")
---
+++ main.py
@@ -0,0 +1,5 @@
+a = "Orca"
+
+print("Selamat Pagi Dunia")
+print("Perkenalkan nama saya Azis")
+print("Saya sekarang sedang mengalamai Butterfly Era bersama" + a)
